920e44c51a3ddf029e7e53608fc172f9cf60a422
Carl Frenn <carl.frenn@gmail.com>
2020-12-08 22:33:13 +0100

    day 7-2 not finished

--- src/main/java/se/tastebin/adventofcode/day07/Day07.java
+++ src/main/java/se/tastebin/adventofcode/day07/Day07.java
@@ -1,108 +1,163 @@
 package se.tastebin.adventofcode.day07;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.stream.Collectors;
 import se.tastebin.adventofcode.ListFromFile;
 
 public class Day07 {
 
     public static void main(String[] args) {
-        List<String> strings = new ListFromFile("src/main/java/se/tastebin/adventofcode/day07/day07.txt").list();
-
-        Map<String, List<String>> bags = new HashMap<>();
+        List<String> strings = new ListFromFile("src/main/java/se/tastebin/adventofcode/day07/day07-test.txt").list();
         
-        for (String row : strings) {
-            
-            String[] rowStuff = row.split("bags contain");
-            
-            String key = rowStuff[0].trim();
-            
-            List<String> other = getOther(rowStuff[1].trim());
-            //System.out.println(key);
-            
-            
-            bags.put(key, other);
-        }
-        
-        
-        //System.out.println(bags);
+        Rules rules = new Rules(strings);
        
-        Set<String> keys = bags.keySet();
+        List<String> keys = rules.keys();
         
         List<Bag> result = new ArrayList<>();
         
         for (String k : keys) {
-            result.add(new Bag(k, bags));
+            result.add(new Bag(k, 0, rules));
         }
         
-        //System.out.println(result);
+        // #1
         
         long count = result.stream().filter(b-> b.hasBag("shiny gold")).count();
-        System.out.println(count);
-    }
-
-    
-    
-    private final static List<String> getOther(String stuff) {
+        System.out.println(String.format("Number of bag colors: %d", count));
         
-        String[] xs = stuff.split(",");
-      
-        List<String> result = new ArrayList<>();
-        for (String x : xs) {
-            String cleaned = cleanup(x);
-            if (!cleaned.isEmpty()) {
-                result.add(cleaned);
-            }
-        }
-        return result;
+        // #2
+        
+        Bag gold = new Bag("shiny gold", 1, rules);
+        System.out.println(gold);
+        System.out.println(String.format("Number of bags: %d ", gold.count()));
     }
     
-    private final static String cleanup(String in) {
-        if ("no other bags.".equals(in.trim())) {
-            return "";
-        } else {
-            String[] xx = in.trim().split(" ");
-            return String.format("%s %s",xx[1], xx[2]);
-        }
-    }
+
    
-    
-    
-    private static class Bag {
+    private static class Rules {
         
-        private final String name;
-        private final List<Bag> content;
+        private final Map<String, List<BagWithAmount>> bags = new HashMap<>();
 
-        public Bag(String name, Map<String, List<String>> bags) {
-            this.name = name;
+        public Rules(List<String> strings) {
+               
+            for (String row : strings) {
             
-            List<String> other = bags.get(name);
-            content = new ArrayList<>();
-            for (String s : other) {
-                content.add(new Bag(s, bags));
-            }
+                String[] rowStuff = row.split("bags contain");
 
+                String key = rowStuff[0].trim();
+
+                List<BagWithAmount> other = getOther(rowStuff[1].trim());
+
+                bags.put(key, other);
+            }
+        }
+        
+        public List<BagWithAmount> get(String name) {
+            return bags.get(name);
+        }
+         
+        public List<String> keys() {
+            return bags.keySet().stream().collect(Collectors.toList());
+        }
+        
+        private List<BagWithAmount> getOther(String stuff) {
+
+            String[] xs = stuff.split(",");
+
+            List<BagWithAmount> result = new ArrayList<>();
+            for (String x : xs) {
+                BagWithAmount cleaned = cleanup(x);
+                if (!cleaned.isEmpty()) {
+                    result.add(cleaned);
+                }
+            }
+            return result;
+        }
+
+        private BagWithAmount cleanup(String in) {
+            if ("no other bags.".equals(in.trim())) {
+                return new BagWithAmount("", 0);
+            } else {
+                String[] xx = in.trim().split(" ");
+                int val = Integer.parseInt(xx[0]);
+                return new BagWithAmount(String.format("%s %s",xx[1], xx[2]), val);
+            }
+        }
+        
+
+    }
+    
+    private static class BagWithAmount {
+        private final String name;
+        private final int amount;
+
+        public BagWithAmount(String name, int amount) {
+            this.name = name;
+            this.amount = amount;
         }
         
         public String name() {
             return name;
         }
         
+        public int amount() {
+            return amount;
+        }
+        
+        public boolean isEmpty() {
+           return "".equals(name) && amount == 0;
+        }
+    }
+    
+    private static class Bag {
+        
+        private final String name;
+        private final int value;
+        private final List<Bag> content;
+
+        public Bag(String name, int value, Rules rules) {
+            this.name = name;
+            this.value = value;
+            
+            List<BagWithAmount> other = rules.get(name);
+            content = new ArrayList<>();
+            for (BagWithAmount bwa : other) {
+                content.add(new Bag(bwa.name(), bwa.amount(), rules));
+            }
+
+        }
+        
         public List<Bag> content() {
             return content;
         }
+        
+        public int value() {
+            return value;
+        }
 
+        public boolean isBag(String name) {
+            return this.name.equals(name);
+        }
+        
         public boolean hasBag(String search) {
-            return content.stream().anyMatch(b -> (b.name().equals(search) || b.hasBag(search)));
+            return content.stream().anyMatch(b -> (b.isBag(search) || b.hasBag(search)));
+        }
+        
+        public long count() {
+           long total = 0;
+           
+           for (Bag b : content) {
+               total = total + b.value() + b.count();
+           }
+            
+           return value * total;
         }
         
         @Override
         public String toString() {
-            return name + content;
+            return String.format("%s %s %s", name, value, content);
         }
         
         
