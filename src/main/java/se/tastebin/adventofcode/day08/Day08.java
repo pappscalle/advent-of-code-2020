bb636bd760ea1fa08ea0ecf85fa430b7ddf9433a
Carl Frenn <carl.frenn@gmail.com>
2020-12-11 00:46:08 +0100

    day 8-2

--- src/main/java/se/tastebin/adventofcode/day08/Day08.java
+++ src/main/java/se/tastebin/adventofcode/day08/Day08.java
@@ -9,29 +9,38 @@ public class Day08 {
     public static void main(String[] args) {
         
         List<String> strings = new ListFromFile("src/main/java/se/tastebin/adventofcode/day08/day08.txt").list();
-
-        int accumulator = 0;
         int current = 0;
-        int steps = 0;
-        Set<Integer> visited = new HashSet<>();
-        while (!visited.contains(current)){
-           visited.add(current);
-           Row row = new Row(strings.get(current));
-           String instr = row.instruction();
-           int value = row.value();
-           switch (instr) {
-               case "nop": current++; break;
-               case "acc": accumulator+=value; current++; break; 
-               case "jmp": current+=value; break;
-           }
-           steps++;
-           System.out.println(String.format("%d: %s %d current: %d acc: %d", steps, instr, value, current, accumulator));
+        int loop = 0;
+        while (current < strings.size()) {
+
+            loop++;
+            current = 0;
+            int accumulator = 0;
+            int steps = 0;
+            Set<Integer> visited = new HashSet<>();
+            while (!visited.contains(current)){     
+               steps++; 
+                
+               visited.add(current);
+               Row row = new Row(strings.get(current));
+               String instr = row.instruction();
+               int value = row.value();
+               if ("jmp".equals(instr) && loop == steps) {
+                   instr = "nop";
+                   
+               }
+               switch (instr) {
+                   case "nop": current++; break;
+                   case "acc": accumulator+=value; current++; break; 
+                   case "jmp": current+=value; break;
+               }
+               System.out.println(String.format("%d: %s %d current: %d acc: %d", steps, instr, value, current, accumulator));
+            }
+
+
         }
-        
-        
+
     }
-    
-    
     private static class Row {
         private final String row;
         
