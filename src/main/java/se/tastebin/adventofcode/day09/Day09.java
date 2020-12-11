b4ee6097ab3b3f2c4f1e0f07a5f69c94ead42726
Carl Frenn <carl.frenn@gmail.com>
2020-12-11 08:41:39 +0100

    day 9 incomplete

--- src/main/java/se/tastebin/adventofcode/day09/Day09.java
+++ src/main/java/se/tastebin/adventofcode/day09/Day09.java
@@ -1,10 +1,18 @@
 package se.tastebin.adventofcode.day09;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import se.tastebin.adventofcode.ListFromFile;
 
 public class Day09 {
     public static void main(String[] args) {
         List<String> strings = new ListFromFile("src/main/java/se/tastebin/adventofcode/day09/day09-test.txt").list();
+        List<Long> numbers = strings.stream().map(s-> Long.valueOf(s)).collect(Collectors.toList());
+        
+        System.out.println(numbers);
+        
+        
+        
+        
     }
 }
