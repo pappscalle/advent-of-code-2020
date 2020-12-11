e6c691d353858ccb003ef36d2b2fdf2a1e81c8e3
Carl Frenn <carl.frenn@gmail.com>
2020-12-11 08:23:24 +0100

    day 9 incomplete

---
+++ src/main/java/se/tastebin/adventofcode/day09/Day09.java
@@ -0,0 +1,10 @@
+package se.tastebin.adventofcode.day09;
+
+import java.util.List;
+import se.tastebin.adventofcode.ListFromFile;
+
+public class Day09 {
+    public static void main(String[] args) {
+        List<String> strings = new ListFromFile("src/main/java/se/tastebin/adventofcode/day09/day09-test.txt").list();
+    }
+}
